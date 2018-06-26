38cf3f7db79f459cf5c05c8ad3947b3f986457bd
liuxingang <909918440@qq.com>
2018-06-26 19:57:30 +0800

    lesson4.1 增加router

--- client/index.js
+++ client/index.js
@@ -1,11 +1,13 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './app.vue'
-
 import './assets/styles/global.styl'
+import createRouter from './config/router'
+Vue.use(VueRouter)
 
-const root = document.createElement('div')
-document.body.appendChild(root)
+const router = createRouter()
 
 new Vue({
+  router,
   render: (h) => h(App)
-}).$mount(root)
+}).$mount('#root')
