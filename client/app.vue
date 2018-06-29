c07a0bdf1b16d6276decab95905b54a1cc4049e4
liuxingang <909918440@qq.com>
2018-06-29 11:06:09 +0800

    lesson4.8 vuex 热更替

--- client/app.vue
+++ client/app.vue
@@ -1,64 +1,70 @@
 <template>
-    <div id="app">
-        <div id="cover"></div>
-        <Header></Header>
-      <p>{{count}}</p>
-      <router-link to="/app">app</router-link>
-      <router-link to="/login">login</router-link>
-      <transition name="fade">
-        <router-view />
-      </transition>
-        <Footer></Footer>
-      <router-view name="a"/>
-    </div>
+  <div id="app">
+    <div id="cover"></div>
+    <Header></Header>
+    <p>{{count}}</p>
+    <p>{{fullName}} {{count}}</p>
+    <router-link to="/app">app</router-link>
+    <router-link to="/login">login</router-link>
+    <transition name="fade">
+      <router-view/>
+    </transition>
+    <Footer></Footer>
+    <router-view name="a"/>
+  </div>
 </template>
 
 <script>
-    import Header from './layout/header.vue'
-    import Footer from './layout/footer.jsx'
+  import {mapState, mapGetters, mapActions, mapMutations} from 'vuex'
+  import Header from './layout/header.vue'
+  import Footer from './layout/footer.jsx'
 
-    console.log(Header.__docs)
-    export default{
-      data () {
-        return {
-          text: 'abcde2'
-        }
-      },
-      mounted () {
-        console.log(this.$store)
-        let i = 1
-        setInterval(() => {
-          this.$store.commit('updateCount', i++)
-        }, 1000)
-      },
-      computed: {
-        count () {
-          return this.$store.state.count
-        }
-      },
-      components: {
-        Header,
-        Footer
+  console.log(Header.__docs)
+  export default {
+    data () {
+      return {
+        text: 'abcde2'
       }
+    },
+    mounted () {
+      console.log(this.$store)
+      this.updateCountAsync({
+        num: 5,
+        time: 2000
+      })
+    },
+    computed: {
+      ...mapState(['count']),
+      ...mapGetters(['fullName'])
+    },
+    methods: {
+      ...mapActions(['updateCountAsync']),
+      ...mapMutations(['updateCount'])
+    },
+    components: {
+      Header,
+      Footer
     }
+  }
 </script>
 
 <style lang="stylus" scoped>
-    #app{
-        position absolute
-        left 0
-        right 0
-        top 0
-        bottom 0
-    }
-    #cover{
-        position absolute
-        left 0
-        top 0
-        right 0
-        bottom 0
-        background-color #999
-        opacity .9
-        z-index -1
-    }
+  #app {
+    position absolute
+    left 0
+    right 0
+    top 0
+    bottom 0
+  }
+
+  #cover {
+    position absolute
+    left 0
+    top 0
+    right 0
+    bottom 0
+    background-color #999
+    opacity .9
+    z-index -1
+  }
 </style>
---
+++ client/store/state/state.js
@@ -0,0 +1,5 @@
+export default {
+  count: 0,
+  firstName: 'liu1',
+  lastName: 'xin12'
+}
